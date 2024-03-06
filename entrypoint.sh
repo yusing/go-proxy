71cd2f394803c0b98cc99eb0c101e6094bcc699d
yusing <bookz2i2@duck.com>
2024-03-07 04:58:15 +0800

    entrypoint fix for no log

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,5 +11,5 @@ if [ "$DEBUG" == "1" ]; then
     /app/go-proxy -v=$VERBOSITY -log_dir=log --stderrthreshold=0 2>> log/go-proxy.log &
     tail -f /dev/null
 else
-    /app/go-proxy -v=$VERBOSITY -log_dir=log --stderrthreshold=0
+    /app/go-proxy -v=$VERBOSITY -log_dir=log --stderrthreshold=0 2>> log/go-proxy.log
 fi
