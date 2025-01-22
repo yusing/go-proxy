fc29e8f9fa4c47a4578a110d2bdb8517ef4e2b94
yusing <yusing@6uo.me>
2025-01-22 08:32:51 +0800

    fix typo

--- internal/net/http/middleware/oidc.go
+++ internal/net/http/middleware/oidc.go
@@ -20,7 +20,7 @@ var OIDC = NewMiddleware[oidcMiddleware]()
 
 func (amw *oidcMiddleware) finalize() error {
 	if !auth.IsOIDCEnabled() {
-		return E.New("OIDC not enabled but ODIC middleware is used")
+		return E.New("OIDC not enabled but OIDC middleware is used")
 	}
 	authProvider, err := auth.NewOIDCProviderFromEnv()
 	if err != nil {
